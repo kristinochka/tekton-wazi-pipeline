eb3d517a03008808f3c3a34068ebb4a76f9b8104
KRISTINA MAYO <ktopchi@us.ibm.com>
2021-11-02 13:36:16 -0400

    Fix the script

--- .tekton/dbb-build.sh
+++ .tekton/dbb-build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -x
-mkdir -p '/u/ibmuser/tekton'
-rm -rf /u/ibmuser/tekton/zopeneditor-sample
-git clone https://github.com/kristinochka/tekton-wazi-pipeline '/u/ibmuser/tekton/tekton-wazi-pipeline'
+cd /u/ibmuser/tekton
 WORKSPACE='/u/ibmuser/tekton'
 DBB_LOG_DIR='/u/ibmuser/tekton/dbb-logs'
+REPO_NAME=tekton-wazi-pipeline
+rm -rf /u/ibmuser/tekton/$REPO_NAME
+git clone https://github.com/kristinochka/$REPO_NAME
 export DBB_HOME='/usr/lpp/IBM/dbb'
-/usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace $WORKSPACE --application zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
+/usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace $WORKSPACE --application $REPO_NAME/zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
