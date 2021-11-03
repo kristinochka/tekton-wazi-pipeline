eddfcde2704f5a6708f733c2c6ec0e598364367f
KRISTINA MAYO <ktopchi@us.ibm.com>
2021-11-02 23:36:03 -0400

    Use full git path to envoke git clone

--- .tekton/dbb-build.sh
+++ .tekton/dbb-build.sh
@@ -2,8 +2,10 @@
 set -x
 export DBB_HOME='/usr/lpp/IBM/dbb'
 WORKSPACE='/u/ibmuser/tekton'
-DBB_LOG_DIR=$WORKSPACE/dbb-logs
-REPO_NAME=tekton-wazi-pipeline
-rm -rf $WORKSPACE/$REPO_NAME
-git clone https://github.com/kristinochka/$REPO_NAME $WORKSPACE/$REPO_NAME
-/usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace $WORKSPACE --application $REPO_NAME/zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
+DBB_LOG_DIR="${WORKSPACE}/dbb-logs"
+REPO_NAME='tekton-wazi-pipeline'
+REPO_PATH="${WORKSPACE}/${REPO_NAME}"
+rm -rf "${REPO_PATH}"
+# export PATH=${PATH}:/rsusr/ported/bin:/usr/lpp/IBM/dbb/bin
+/rsusr/ported/bin/git clone https://github.com/kristinochka/${REPO_NAME} ${REPO_PATH}
+/usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace ${WORKSPACE} --application ${REPO_NAME}/zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
