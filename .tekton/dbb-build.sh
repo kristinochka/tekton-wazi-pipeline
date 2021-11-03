789ee032523ecbeee9d73f152da4bdbd65bed22f
KRISTINA MAYO <ktopchi@us.ibm.com>
2021-11-02 22:28:22 -0400

    Execute job from home directory

--- .tekton/dbb-build.sh
+++ .tekton/dbb-build.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 set -x
-cd /u/ibmuser/tekton
-WORKSPACE='/u/ibmuser/tekton'
-DBB_LOG_DIR='/u/ibmuser/tekton/dbb-logs'
-REPO_NAME=tekton-wazi-pipeline
-rm -rf /u/ibmuser/tekton/$REPO_NAME
-git clone https://github.com/kristinochka/$REPO_NAME
 export DBB_HOME='/usr/lpp/IBM/dbb'
+WORKSPACE='/u/ibmuser/tekton'
+DBB_LOG_DIR=$WORKSPACE/dbb-logs
+REPO_NAME=tekton-wazi-pipeline
+rm -rf $WORKSPACE/$REPO_NAME
+git clone https://github.com/kristinochka/$REPO_NAME $WORKSPACE/$REPO_NAME
 /usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace $WORKSPACE --application $REPO_NAME/zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
