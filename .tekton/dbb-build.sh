01fec1916738086b9b93605a61649d6325bbe994
KRISTINA MAYO <ktopchi@us.ibm.com>
2021-11-02 23:54:06 -0400

    echo PATH

--- .tekton/dbb-build.sh
+++ .tekton/dbb-build.sh
@@ -6,6 +6,7 @@ DBB_LOG_DIR="${WORKSPACE}/dbb-logs"
 REPO_NAME='tekton-wazi-pipeline'
 REPO_PATH="${WORKSPACE}/${REPO_NAME}"
 rm -rf "${REPO_PATH}"
+echo $PATH
 # export PATH=${PATH}:/rsusr/ported/bin:/usr/lpp/IBM/dbb/bin
 /rsusr/ported/bin/git clone https://github.com/kristinochka/${REPO_NAME} ${REPO_PATH}
 /usr/lpp/IBM/dbb/bin/groovyz -DBB_PERSONAL_DAEMON /u/ibmuser/tekton/dbb-zappbuild/build.groovy --workspace ${WORKSPACE} --application ${REPO_NAME}/zopeneditor-sample --hlq IBMUSER.SAMPLE --outDir $DBB_LOG_DIR --fullBuild
